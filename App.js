ef66a2d2378d51a7e7b5dece6b646794034a2f88
Charlie Delgado <charliedelgado12@icloud.com>
2023-11-07 14:23:33 -0400

    feat: added functionality to reset the timer to its previous state when it reaches 0

--- App.js
+++ App.js
@@ -22,6 +22,13 @@ export default function App() {
     } else {
       clearInterval(interval);
     }
+    
+    if (time === 0) {
+      setIsActive(false);
+      setIsWorking(prev => !prev);
+      setTime(isWorking ? 300 : 1500);
+    };
+
     return () => clearInterval(interval);
   }, [isActive, time]);
 
